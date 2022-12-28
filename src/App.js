0a9e6c3d01360d1fffc8dea2077134ad50f14030
Ianderson-rgb <afamilyenterprises@gmail.com>
2022-12-28 18:01:58 -0500

    Added API and scrollbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
-import { robots } from './robots';
 import SearchBox from './SearchBox';
+import Scroll from	'./Scroll';
 import './App.css';
 
 
@@ -9,10 +9,17 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: '',
 		};
 	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then((response) => response.json())
+		.then(users => this.setState({ robots: users }));
+	}
+
 	onSearchChange = (event) => {
 		this.setState({ searchfield: event.target.value });
 	};
@@ -25,9 +32,11 @@ class App extends Component {
 		});
 		return (
 			<div className='tc'>
-				<h1>RoboFriends</h1>
+				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange} />
+				<Scroll>
 				<CardList robots={filteredRobots} />
+				</Scroll>
 			</div>
 		);
 	}
