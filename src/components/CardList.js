dfbef1af4ced62526049f5a53ddc67429dff8e8c
Ianderson-rgb <afamilyenterprises@gmail.com>
2022-12-30 12:56:23 -0500

    adding card detail toggle

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import Card from './Card';
 
 const CardList = ({ robots }) => {
 	// example of destructoring the prop as a peram.
+	const [crdState, setCrdState] = useState(false);
+
+	function handleClick() {
+		setCrdState((crdState) => !crdState);
+	}
+
+	let toglleClassCheck = crdState ? 'dn' : '';
+	console.log('Toggled', toglleClassCheck);
+	if (toglleClassCheck.includes('dn')) {
+		return (
+			<div onClick={handleClick}>
+				{robots.map((robot, i) => {
+					return (
+						<Card
+							key={robots[i].id}
+							id={robots[i].id}
+							name={robots[i].name}
+							email={robots[i].email}
+						/>
+					);
+				})}
+			</div>
+		);
+	}
 	return (
-		<div>
+		<div 
+		className={`${toglleClassCheck}`} 
+		onClick={handleClick}>
 			{robots.map((robot, i) => {
 				return (
 					<Card
