1ddb38568bde94c328c4ed99c75de47217ea689e
Ianderson-rgb <afamilyenterprises@gmail.com>
2022-12-30 16:47:55 -0500

    commiting final modifications

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,44 +1,22 @@
 import React from "react";
-import { useState } from "react";
 import Card from './Card';
+
 
 const CardList = ({ robots }) => {
 	// example of destructoring the prop as a peram.
-	const [crdState, setCrdState] = useState(false);
-
-	function handleClick() {
-		setCrdState((crdState) => !crdState);
-	}
-
-	let toglleClassCheck = crdState ? 'dn' : '';
-	console.log('Toggled', toglleClassCheck);
-	if (toglleClassCheck.includes('dn')) {
-		return (
-			<div onClick={handleClick}>
-				{robots.map((robot, i) => {
-					return (
-						<Card
-							key={robots[i].id}
-							id={robots[i].id}
-							name={robots[i].name}
-							email={robots[i].email}
-						/>
-					);
-				})}
-			</div>
-		);
-	}
 	return (
-		<div 
-		className={`${toglleClassCheck}`} 
-		onClick={handleClick}>
+		<div>
 			{robots.map((robot, i) => {
 				return (
 					<Card
 						key={robots[i].id}
 						id={robots[i].id}
 						name={robots[i].name}
+						phone={robots[i].phone}
 						email={robots[i].email}
+						company={robots[i].company}
+						website={robots[i].website}
+						address={robots[i].address}
 					/>
 				);
 			})}
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+
 const SearchBox = ({searchChange}) => {
   console.log(searchChange)
   return (
     <div className='pa2'>
-    <input className='pa3 ba b--green bg-lightest-blue'
+    <input 
+    className='pa3 br2 ba b--green bg-lightest-blue'
     type='search' 
     placeholder='search robots'
     onChange={searchChange}
