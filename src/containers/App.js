f4431d085639a021f1d56a291207e302f454a7ab
Ianderson-rgb <afamilyenterprises@gmail.com>
2023-08-04 17:27:04 -0400

    first commit to branch dev-redux-intigration

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,57 @@
-import React, { useState, useEffect } from "react";
-import CardList from "../components/CardList";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setSearchField, requestRobots } from '../actions';
+
+import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from	'../components/Scroll';
-import ErrorBoundary from "../components/ErrorBoundary";
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
 import './App.css';
 
+// parameter state comes from index.js provider store state(rootReducers)
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending
+  }
+}
 
-function App() {
-	
-const [robots, setRobots] = useState([]);
-const [searchfield, setSearchfield] = useState('');
-const [count, setCount] = useState('');
-	useEffect(()=> {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((response) => response.json())
-			.then((users) => setRobots(users));
-			//console.log(count);
-	}, []) // The empty array is important here. It prevents useEffect from running everytime our App() is returned.
-				// If you add count to the array, only useEffect only runs if count changes.
-	const onSearchChange = (event) => {
-		setSearchfield(event.target.value);
-	};
+// dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
+// the function returns an object then uses connect to change the data from redecers.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
 
-		const filteredRobots = robots.filter((robot) => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		});
-		return !robots.length ? (
-			<h1>..Loading</h1>
-		) : (
-			<div className='tc'>
-				<h1 className='neon' data-text='U'>ROB<span 
-				className='flicker-slow'>O</span>FRI<span 
-				className='flicker-fast'>E</span>NDS</h1>
-				<button onClick={()=>setCount(count+1)}>Click Me</button>
-				<SearchBox searchChange={onSearchChange} />
-				<Scroll>
-					<ErrorBoundary>
-						<CardList robots={filteredRobots} />
-					</ErrorBoundary>
-				</Scroll>
-			</div>
-		);
-	}
+class App extends Component {
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
 
-export default App;
+  render() {
+    const { robots, searchField, onSearchChange, isPending } = this.props;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          { isPending ? <h1>Loading</h1> :
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          }
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+// action done from mapDispatchToProps will channge state from mapStateToProps
+export default connect(mapStateToProps, mapDispatchToProps)(App)
